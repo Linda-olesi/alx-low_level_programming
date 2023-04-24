36ac2a24ff2f5039d2fa643c4b834090a3b9f88e
Linda-olesi <lindaateka95@gmail.com>
2023-04-24 11:47:10 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int n = 97;
 
-	while (n <= 123)
+	while (n <= 122)
 	{
 		if (n == 101 || n == 113)
 		{
@@ -17,5 +17,6 @@ int main(void)
 		putchar(n);
 		n++;
 	}
+	putchar('\n');
 	return (0);
 }
